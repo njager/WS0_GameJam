d75376233980dffaa5c8f892cfec0617b715c373
njager <59936380+njager@users.noreply.github.com>
2021-08-29 13:15:22 -0400

    added comments and some basic variables

--- WS0_GameJam/Assets/Scripts/Jager - Scripts/TileController.cs
+++ WS0_GameJam/Assets/Scripts/Jager - Scripts/TileController.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class TileController : MonoBehaviour
 {
+    //member variable
+    private int freeTiles;
+
+
+    //public variables
+    public bool isSearching;
+    public float opinion;
+    public float intelligence;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        opinion = 50f;
+        intelligence = 0f;
     }
 
     // Update is called last every frame
     void Update()
     {
-
+        intelligence += 1 * Time.deltaTime;
+        
+        //check if left mouse button clicked
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("mouse clicked!");
+            //send out a raycast at mouse location
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-
+            //register raycast hits
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
             if (hit.collider != null)
             {
+                //if raycast hit a tile, destroy it
                 Debug.Log("Something was clicked!");
                 Debug.Log(hit.collider.gameObject.name);
                 if(hit.collider.gameObject.tag == "Tile")
