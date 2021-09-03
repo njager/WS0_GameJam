44a081a90b272ba93850aadd2d2302e3711c84e6
Josh Tonner <jtonner@iu.edu>
2021-09-03 06:08:24 -0400

    Dialogue system variabled and now fully scripted

--- WS0_GameJam/Assets/Scripts/Josh - Scripts/DialogueSystem.cs
+++ WS0_GameJam/Assets/Scripts/Josh - Scripts/DialogueSystem.cs
@@ -61,37 +61,168 @@ public class DialogueSystem : MonoBehaviour
     {
         if (global.freeTiles1 == 3)
         {
-
+            frame1Log1.SetActive(true);
+            frame1Log2.SetActive(false);
+            frame1Log3.SetActive(false);
         }
+
+        if (global.freeTiles1 == 0)
+        {
+            frame1Log1.SetActive(false);
+            frame1Log2.SetActive(true);
+            frame1Log3.SetActive(false);
+        }
+
+        if (global.freeTiles1 < 0)
+        {
+            frame1Log1.SetActive(false);
+            frame1Log2.SetActive(false);
+            frame1Log3.SetActive(true);
+        }
+
     }
 
     public void DialogueFrameCheck2()
     {
+        if (global.freeTiles2 == 3)
+        {
+            frame2Log1.SetActive(true);
+            frame2Log2.SetActive(false);
+            frame2Log3.SetActive(false);
+        }
 
+        if (global.freeTiles2 == 0)
+        {
+            frame2Log1.SetActive(false);
+            frame2Log2.SetActive(true);
+            frame2Log3.SetActive(false);
+        }
+
+        if (global.freeTiles2 < 0)
+        {
+            frame2Log1.SetActive(false);
+            frame2Log2.SetActive(false);
+            frame2Log3.SetActive(true);
+        }
     }
 
     public void DialogueFrameCheck3()
     {
+        if (global.freeTiles3 == 3)
+        {
+            frame3Log1.SetActive(true);
+            frame3Log2.SetActive(false);
+            frame3Log3.SetActive(false);
+        }
 
+        if (global.freeTiles3 == 0)
+        {
+            frame3Log1.SetActive(false);
+            frame3Log2.SetActive(true);
+            frame3Log3.SetActive(false);
+        }
+
+        if (global.freeTiles3 < 0)
+        {
+            frame1Log3.SetActive(false);
+            frame1Log3.SetActive(false);
+            frame1Log3.SetActive(true);
+        }
     }
 
     public void DialogueFrameCheck4()
     {
+        if (global.freeTiles4 == 3)
+        {
+            frame4Log1.SetActive(true);
+            frame4Log2.SetActive(false);
+            frame4Log3.SetActive(false);
+        }
 
+        if (global.freeTiles4 == 0)
+        {
+            frame4Log1.SetActive(false);
+            frame4Log2.SetActive(true);
+            frame4Log3.SetActive(false);
+        }
+
+        if (global.freeTiles4 < 0)
+        {
+            frame4Log1.SetActive(false);
+            frame4Log2.SetActive(false);
+            frame4Log3.SetActive(true);
+        }
     }
 
     public void DialogueFrameCheck5()
     {
+        if (global.freeTiles5 == 3)
+        {
+            frame5Log1.SetActive(true);
+            frame5Log2.SetActive(false);
+            frame5Log3.SetActive(false);
+        }
 
+        if (global.freeTiles5 == 0)
+        {
+            frame5Log1.SetActive(false);
+            frame5Log2.SetActive(true);
+            frame5Log3.SetActive(false);
+        }
+
+        if (global.freeTiles5 < 0)
+        {
+            frame5Log1.SetActive(false);
+            frame5Log2.SetActive(false);
+            frame5Log3.SetActive(true);
+        }
     }
 
     public void DialogueFrameCheck6()
     {
+        if (global.freeTiles6 == 3)
+        {
+            frame6Log1.SetActive(true);
+            frame6Log2.SetActive(false);
+            frame6Log3.SetActive(false);
+        }
 
+        if (global.freeTiles6 == 0)
+        {
+            frame6Log1.SetActive(false);
+            frame6Log2.SetActive(true);
+            frame6Log3.SetActive(false);
+        }
+
+        if (global.freeTiles6 < 0)
+        {
+            frame6Log1.SetActive(false);
+            frame6Log2.SetActive(false);
+            frame6Log3.SetActive(true);
+        }
     }
 
     public void DialogueFrameCheck7()
     {
+        if (global.freeTiles7 == 3)
+        {
+            frame7Log1.SetActive(true);
+            frame7Log2.SetActive(false);
+            frame7Log3.SetActive(false);
+        }
 
+        if (global.freeTiles7 == 0)
+        {
+            frame7Log1.SetActive(false);
+            frame7Log2.SetActive(true);
+            frame7Log3.SetActive(false);
+        }
+
+        if (global.freeTiles7 < 0)
+        {
+            frame7Log1.SetActive(false);
+            frame7Log2.SetActive(false);
+            frame7Log3.SetActive(true);
+        }
     }
 }
