ab9861121f53f9764da98bc83635e77451a4052e
Josh Tonner <jtonner@iu.edu>
2021-09-03 02:57:10 -0400

    Setting up scripts for each tilebox

---
+++ WS0_GameJam/Assets/Scripts/Josh - Scripts/CharacterFileScripts/CharacterFileScript3.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFileScript3 : MonoBehaviour
+{
+    private GlobalController global;
+
+    public GameObject tileCover1;
+    public GameObject tileCover2;
+    public GameObject tileCover3;
+    public GameObject tileCover4;
+    public GameObject tileCover5;
+    public GameObject tileCover6;
+    public GameObject tileCover7;
+    public GameObject tileCover8;
+    public GameObject tileCover9;
+
+
+    void Start()
+    {
+        tileCover1.SetActive(true);
+        tileCover2.SetActive(true);
+        tileCover3.SetActive(true);
+        tileCover4.SetActive(true);
+        tileCover5.SetActive(true);
+        tileCover6.SetActive(true);
+        tileCover7.SetActive(true);
+        tileCover8.SetActive(true);
+        tileCover9.SetActive(true);
+
+        global = GlobalController.instance;
+    }
+
+    public void tileFlip1()
+    {
+        global.freeTiles3--;
+        tileCover1.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip2()
+    {
+        global.freeTiles3--;
+        tileCover2.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip3()
+    {
+        global.freeTiles3--;
+        tileCover3.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip4()
+    {
+        global.freeTiles3--;
+        tileCover4.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip5()
+    {
+        global.freeTiles3--;
+        tileCover5.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip6()
+    {
+        global.freeTiles3--;
+        tileCover6.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip7()
+    {
+        global.freeTiles3--;
+        tileCover7.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip8()
+    {
+        global.freeTiles3--;
+        tileCover8.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip9()
+    {
+        global.freeTiles3--;
+        tileCover9.SetActive(false);
+        global.cardFlip.Play();
+    }
+
+
+
+}
---
+++ WS0_GameJam/Assets/Scripts/Josh - Scripts/CharacterFileScripts/CharacterFileScript4.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFileScript4 : MonoBehaviour
+{
+    private GlobalController global;
+
+    public GameObject tileCover1;
+    public GameObject tileCover2;
+    public GameObject tileCover3;
+    public GameObject tileCover4;
+    public GameObject tileCover5;
+    public GameObject tileCover6;
+    public GameObject tileCover7;
+    public GameObject tileCover8;
+    public GameObject tileCover9;
+
+
+    void Start()
+    {
+        tileCover1.SetActive(true);
+        tileCover2.SetActive(true);
+        tileCover3.SetActive(true);
+        tileCover4.SetActive(true);
+        tileCover5.SetActive(true);
+        tileCover6.SetActive(true);
+        tileCover7.SetActive(true);
+        tileCover8.SetActive(true);
+        tileCover9.SetActive(true);
+
+        global = GlobalController.instance;
+    }
+
+    public void tileFlip1()
+    {
+        global.freeTiles4--;
+        tileCover1.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip2()
+    {
+        global.freeTiles4--;
+        tileCover2.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip3()
+    {
+        global.freeTiles4--;
+        tileCover3.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip4()
+    {
+        global.freeTiles4--;
+        tileCover4.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip5()
+    {
+        global.freeTiles4--;
+        tileCover5.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip6()
+    {
+        global.freeTiles4--;
+        tileCover6.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip7()
+    {
+        global.freeTiles4--;
+        tileCover7.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip8()
+    {
+        global.freeTiles4--;
+        tileCover8.SetActive(false);
+        global.cardFlip.Play();
+    }
+    public void tileFlip9()
+    {
+        global.freeTiles4--;
+        tileCover9.SetActive(false);
+        global.cardFlip.Play();
+    }
+
+
+
+}
